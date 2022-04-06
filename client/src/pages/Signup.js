822443be58b2d37b13253b20450c18f52c8f42b4
Scott Rohrig <scott.rohrig@gmail.com>
2022-04-06 12:00:38 -0700

    basic structure and pseudo code

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+// import mutation hook
+// import Link for router
+// import adding user mutation
+// import auth class to handle json web token decoding
+
+
+// define Login component function
+function Signup(props) {
+
+  // set the default form state
+  const [ formState, setFormState ] = useState({
+    email: 'example@email.com',
+    password: '********'
+  })
+  // define the login function to handle the LOGIN mutation
+
+  // form submit handler function
+
+  // form input change handler function
+
+  // return the component JSX
+  return (
+    <div>
+      {/*
+        form with styling (see stylesheet for class names)
+          label
+          input
+          input
+          input
+          button
+        link to login page
+        error message
+      */}
+
+    </div>
+  )
+
+}

917275983cf917184ba7e47cafe2541f67cbb258
Sparkle Party <lexi.slovik@gmail.com>
2022-04-06 14:08:48 -0700

    add ApolloServer and middleware to server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,34 @@
 const path = require( 'path' );
 const express = require( 'express' );
-// ApolloServer ⇒
+const { ApolloServer } = require('apollo-server-express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// require schemas here ↓
-// require middleware
+const { typeDefs, resolvers } = require('./schemas');
 const db = require( './config/connection' );
+const { authMiddleware } = require('./utils/auth');
 
 // create Apollo server instance and config
+const startServer = async () => {
+  // create new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+
+  // Start the Apollo server
+  await server.start();
+
+  // integrate our Apollo server with the Express app as middleware
+  server.applyMiddleware({ app });
+
+  // log URL to test GraphQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
 
 // initialize apollo server
+startServer();
 
 app.use( express.urlencoded( { extended: false } ) );
 app.use( express.json() );

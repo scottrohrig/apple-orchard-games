e4eff9793b693e5a70c991803ff2b063f6a0079e
Sparkle Party <lexi.slovik@gmail.com>
2022-04-06 14:18:57 -0700

    add basic typeDefs to test JWT in graphQL

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,14 @@
 // require apollo server
+const { AuthenticationError } = require('apollo-server-express');
 // require necessary models
+const { User } = require('../models');
 // require auth
+const { signToken } = require('../utils/auth');
 
 // define resolvers
+const resolvers = {
+
+};
 
 // modularize resolvers
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,32 @@
 // import gql
+const { gql } = require('apollo-server-express');
 
 // define typeDefs
+const typeDefs = gql `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        money: Int
+        appleCount: Int
+        gemCount: Int
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        users: [User]
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+    }
+`;
 
 // modularize typeDefs
+module.exports = typeDefs;

82944e2254e5f082014b0b3f9cf56808bfd9cd1e
Sparkle Party <lexi.slovik@gmail.com>
2022-04-06 14:06:20 -0700

    add middleware to utils/auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,5 +1,44 @@
 // require jwt
+const jwt = require('jsonwebtoken');
 
 // create secret and expiration
+const secret = 'HowBoutThemApples';
+const expiration = '2h';
 
 // modularize with middleware functions
+module.exports = {
+    // signToken with user information
+    signToken: function({ username, email, _id }) {
+        const payload = { username, email, _id };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+
+    // set up authMiddleware function
+    authMiddleware: function({ req }) {
+        // allow token to be sent via req.body, req.query, or req.headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        // remove "Bearer" in authorization header
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        // if no token, return req object
+        if (!token) {
+            return req;
+        }
+
+        try {
+            // destructure data, store in req object
+            // check that secret matches jwt.sign() token
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+        }
+
+        // return updated req object
+        return req;
+    }
+};

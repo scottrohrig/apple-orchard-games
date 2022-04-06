19983f993705731061d9b14045ef26a4f4acbd3e
Sparkle Party <lexi.slovik@gmail.com>
2022-04-06 14:20:32 -0700

    add basic user resolvers to test JWT

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,6 +7,41 @@ const { signToken } = require('../utils/auth');
 
 // define resolvers
 const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            // create new user in db w/ args passed in
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if(!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+    }
 
 };
 

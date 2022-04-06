5689cad557baf82987b605dd363e54c3b5fe0f86
stuart-rickard <stuart@bau-dev.com>
2022-04-05 22:32:13 -0700

    tree and masher

---
+++ client/src/components/Tree.js
@@ -0,0 +1,11 @@
+import React from "react";
+import treeImage from "../assets/images/tree.jpg";
+
+export default function Tree() {
+  return (
+    <>
+      <h1>tree</h1>
+      <img src={treeImage}></img>
+    </>
+  );
+}

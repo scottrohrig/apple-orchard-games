665614278b8cf50286d2ed4ee7c23613b2be0c33
stuart-rickard <stuart@bau-dev.com>
2022-04-08 14:10:41 -0700

    gameVariables object added to GlobalProvider function

--- client/src/utils/GobalState.js
+++ client/src/utils/GobalState.js
@@ -1,19 +1,39 @@
-import React, { createContext, useContext } from 'react'
-import { useAppReducer } from './reducers'
+import React, { createContext, useContext } from "react";
+import { useAppReducer } from "./reducers";
 
-const GlobalContext = createContext()
+const GlobalContext = createContext();
 const { Provider } = GlobalContext;
 
-const GlobalProvider = ({ value=[], ...props})=>{
+const GlobalProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useAppReducer({
-    mashers: [{},{},{},{},{}],
-    juicers: [{_id: 1},{},{},{},{}],
-    ovens: [{},{},{},{},{}],
-    trees: [{},{},{},{},{}],
+    gameVariables: {
+      initialAppleInventory: 5,
+      applesForNewTree: 3,
+      appleGrowTime: 30,
+      appleSaleRevenue: 1,
+      juicerCost: 10,
+      masherCost: 14,
+      ovenCost: 30,
+      makeJuiceTime: 60,
+      makeJuiceApplesUsed: 2,
+      sauceMashTime: 120,
+      sauceMashApplesUsed: 4,
+      pieBakeTime: 300,
+      pieBakeApplesUsed: 8,
+      juiceSaleRevenue: 4,
+      sauceSaleRevenue: 8,
+      pieSaleRevenue: 20,
+      gemPurchaseCost: 0.99,
+      gemsFromPurchase: 5,
+    },
+    mashers: [{}, {}, {}, {}, {}],
+    juicers: [{ _id: 1 }, {}, {}, {}, {}],
+    ovens: [{}, {}, {}, {}, {}],
+    trees: [{}, {}, {}, {}, {}],
     orchards: [
       {
         _id: 1,
-        trees: []
+        trees: [],
       },
     ],
     currentOrchard: [],
@@ -26,4 +46,4 @@ const useGlobalContext = () => {
   return useContext(GlobalContext);
 };
 
-export { GlobalProvider, useGlobalContext}
+export { GlobalProvider, useGlobalContext };

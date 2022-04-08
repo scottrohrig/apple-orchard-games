2e650bae14d533e431b19ea78d63da8eadc86def
Scott Rohrig <scott.rohrig@gmail.com>
2022-04-07 20:04:28 -0700

    app reducer

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,16 +1,19 @@
+import { useReducer } from 'react';
 import { UPDATE_TIMERS } from './actions';
 
-export const reducer = (state=[],action) => {
-  console.log(state, action);
+export const reducer = (state = [], action) => {
   switch (action.type) {
     case UPDATE_TIMERS:
       return state.map((timer) => {
-        console.log('reducer timer before',timer);
         if (timer.isRunning) {
-          timer = { ...timer, startedAtTime: timer.startedAtTime += action.payload.deltaTime }
+          timer = { ...timer, startedAtTime: timer.startedAtTime += action.payload.deltaTime };
         }
-        console.log('reducer timer after',timer);
-        return timer
-      })
+        return timer;
+      });
   }
+};
+
+// initialize our global state object
+export function useAppReducer(initialState) {
+  return useReducer(reducer, initialState)
 }

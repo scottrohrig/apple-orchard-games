67080ce06fa9d00664306681ab75e5c1cb61229d
Charlie H <charltonhua@gmail.com>
2022-04-07 22:33:55 -0700

    add some queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -8,12 +8,63 @@ export const QUERY_ITEMS = gql`
 
 // me
 
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      money
+      appleCount
+      gemCount
+      orchards {
+        _id
+      }
+      trees {
+        _id
+      }
+      juicers {
+        _id
+      }
+      mashers {
+        _id
+      }
+      ovens {
+        _id
+      }
+    }
+  }
+`;
+
 // get orchards
+export const GET_ORCHARDS = gql`
+  query {
+
+  }
+`;
 
 // get mashers
+export const GET_MASHERS = gql`
+  query {
+
+  }
+`;
 
 // get juicers
+export const GET_JUICERS = gql`
+  query {
 
+  }
+`;
 // get ovens
+export const GET_OVENS = gql`
+  query {
 
+  }
+`;
 // get trees
+export const GET_TREES = gql`
+  query {
+
+  }
+`;

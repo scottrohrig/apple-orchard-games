3c5096c3fc106ad03e42957865357384562d4e11
Scott Rohrig <scott.rohrig@gmail.com>
2022-04-08 22:59:59 -0700

    add custom useInterval Hook, getTimeRemaining function

---
+++ client/src/utils/helpers.js
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useState } from 'react';
+
+export function getTimeRemaining(startedAtTime, duration) {
+  const now = new Date()
+  return duration - Math.floor((now - startedAtTime) / 1000)
+}
+
+export function formatTime(rawTime) {
+  var minutes = rawTime.getMinutes()
+  var seconds = rawTime.getSeconds()
+  var strTime = `${seconds}`
+
+  return strTime
+}
+
+// custom interval Hook
+export function useInterval(callback, delay) {
+  const savedCallback = useRef();
+  // remember last callback
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // setup interval
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}

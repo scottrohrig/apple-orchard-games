08620e2b5a06fff23bb956062a61165fa7b94414
Scott Rohrig <scott.rohrig@gmail.com>
2022-04-09 14:58:14 -0700

    weight seeded user values by level

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -9,13 +9,15 @@ db.once('open', async () => {
   // create user data
   const userData = [];
 
+  const weight = Math.floor(Math.random() * 10 )
+
   for (let i = 0; i < 10; i += 1) {
     const username = faker.internet.userName();
     const email = faker.internet.email(username);
     const password = 'pw123';
-    const gemCount = Math.floor(Math.random() * 30);
-    const appleCount = Math.floor(Math.random() * 30);
-    const money = Math.floor(Math.random() * 100);
+    const gemCount = Math.floor(Math.random() * (1*weight));
+    const appleCount = Math.floor(Math.random() * (50 * weight));
+    const money = Math.floor(Math.random() * (250 * weight));
 
     userData.push({ username, email, password, gemCount, money, appleCount });
   }

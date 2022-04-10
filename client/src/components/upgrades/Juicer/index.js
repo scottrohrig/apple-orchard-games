f2b12c63212c568241b3d0f25c4eb214486eff27
Scott Rohrig <scott.rohrig@gmail.com>
2022-04-10 11:14:33 -0700

    import update action, deconstruct props, dispatch startedAtTime

--- client/src/components/upgrades/Juicer/index.js
+++ client/src/components/upgrades/Juicer/index.js
@@ -2,27 +2,24 @@ import '../item.css';
 import icon from '../../../assets/images/juicer.png';
 import { useState } from 'react';
 import { formatTime, getTimeRemaining, useInterval } from '../../../utils/helpers';
+import { UPDATE_JUICERS } from '../../../utils/actions';
 
-const start = new Date();
-
-const mock = {
-  _id: 1,
-  startedAtTime: start,
-  duration: 6,
-}
 
 // pass in juicer props from parent page / component
-const Juicer = (juicer) => {
+const Juicer = ({props}) => {
 
+  const {juicer, dispatch} = props
   // deconstruct the juicer props passed in from parent
-  const [{ _id, startedAtTime, duration }, setState] = useState(mock) // juicer
+  // const [{ _id, startedAtTime, duration }, setState] = useState(mock) // juicer
+
+  const { _id, startedAtTime, duration } = juicer
 
   const [timeRemaining, setTime] = useState(duration);
   const isReady = timeRemaining <= 0
 
   // custom Hook to handle intervals across item types
   useInterval(() => {
-    if (timeRemaining <= 0) {
+    if (isReady) {
       return;
     }
     setTime(getTimeRemaining(startedAtTime, duration));
@@ -30,7 +27,14 @@ const Juicer = (juicer) => {
 
     const handleUseBtnPressed = (event) => {
       // dispatch update juicer with a new startedAtTime
-      setState({_id, startedAtTime: new Date(), duration})
+      // setState({_id, startedAtTime: new Date(), duration})
+      const now = new Date()
+      console.log('new time', now)
+      dispatch({
+        type: UPDATE_JUICERS,
+        payload:
+        {_id, startedAtTime: now, duration}}
+        )
 
       console.log('dispatching startedAtTime', startedAtTime)
 
@@ -46,7 +50,7 @@ const Juicer = (juicer) => {
         <div className='item-btn-wrapper'>
 
           <div className="item-btn-flex">
-            {timeRemaining <= 0 ? (
+            {isReady ? (
               <button className="btn btn-harvest" onClick={() => { handleUseBtnPressed(); }}>use</button>
             ) : (
               <button className="btn btn-timer" disabled>{timeRemaining}s</button>

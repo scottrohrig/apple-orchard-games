025249ce445fd3347d98a773a5f0971f75564763
Scott Rohrig <scott.rohrig@gmail.com>
2022-04-10 11:13:44 -0700

    pass juicer object and dispatch to Juicer component

--- client/src/components/upgrades/index.js
+++ client/src/components/upgrades/index.js
@@ -11,6 +11,7 @@ export function JuicersRow() {
 
   // destructure the items list from the global state object
   const { juicers } = state;
+  // console.log('state', state, 'juicers', juicers);
 
   // get items data from db
   // const { loading, data: itemData } = useQuery(QUERY_ITEMS);
@@ -30,7 +31,6 @@ export function JuicersRow() {
   }
 
   //
-  console.log(juicers)
   // should the responsibility be in the row or the item
   return (
     <div className='item-row'>
@@ -38,11 +38,12 @@ export function JuicersRow() {
       <span className='item-label'>Juicers</span>
       <div className='item-scroll'>
         {/* map juicers here */}
-        {juicers.map((juicer, i) => (
+        {juicers.map((juicer, i) => {
+          return (
           <div key={i} className='item-box'>
             {juicer._id ? (
               // <img src={require('../../assets/images/juicer.png')}></img>
-              <Juicer />
+              <Juicer props={{juicer, dispatch}} />
               ): (
                 // placeholder
                 // <PlaceHolder />
@@ -53,7 +54,7 @@ export function JuicersRow() {
             }
           </div>
 
-        ))}
+        )})}
       </div>
     </div>
 

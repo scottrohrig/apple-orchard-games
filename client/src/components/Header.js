5080a9574a5c8516b702ecffc3fbe9c132ca3b77
Scott Rohrig <scott.rohrig@gmail.com>
2022-04-11 13:33:11 -0700

    add links to dashboard and shop

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,17 @@
 import React from "react";
 import gem from "../assets/images/gem.svg";
 import basket from "../assets/images/basket.svg";
+import { useGlobalContext } from '../utils/GlobalState';
+import {Link} from 'react-router-dom'
 
 function Header() {
+
+  const [state, dispatch] = useGlobalContext()
+  // const money = state.money
+  const {money, gemCount, appleCount} = state
+  // const gemCount = state.gemCount
+  // const appleCount = state.appleCount
+
   return (
     <div className="page-links">
       <header className="component-header">
@@ -10,19 +19,19 @@ function Header() {
 
             <div className="disp-currency disp-currency-img">
               <img src={gem} alt="gem" />
-              <span>10</span>
+              <span>{gemCount}</span>
             </div>
 
             <div className="disp-currency disp-currency-img">
               <img src={basket} alt="basket of apples" />
-              <span>530</span>
+              <span>{appleCount}</span>
             </div>
 
 
             <div className="disp-currency disp-currency-money">
-              <p className="display-money">100</p>
+              <p className="display-money">{money}</p>
             </div>
-            
+
             <div className="disp-user">
               <button className="btn btn-profile">
                 <i className="fa-solid fa-circle-user"></i>
@@ -33,12 +42,12 @@ function Header() {
       </header>
 
       <div className="nav-buttons">
-        <button className="btn btn-nav">
+        <Link to='/home' className="btn btn-nav">
           <i className="fa-solid fa-house-chimney-window"></i>
-        </button>
-        <button className="btn btn-nav">
+        </Link >
+        <Link to='/marketplace' className="btn btn-nav">
           <i className="fa-solid fa-cart-shopping"></i>
-        </button>
+        </Link >
       </div>
     </div>
   );

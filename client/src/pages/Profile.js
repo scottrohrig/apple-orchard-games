7461b5696c219a5306e019d297529dbdc3a7c7e7
Charlie H <charltonhua@gmail.com>
2022-04-14 19:50:37 -0700

    update profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,9 @@
 import { useQuery, useMutation } from '@apollo/client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Auth from '../utils/auth';
 import { QUERY_ME } from '../utils/queries';
 import { UPDATE_USER } from '../utils/mutations';
+import { validateEmail } from '../utils/helpers';
 
 export default function Profile({
   showProfile,
@@ -19,7 +20,22 @@ export default function Profile({
   const [updateDB, { error }] = useMutation(UPDATE_USER);
 
   const user = data?.me || data?.user || {};
-  console.log(user);
+  // console.log(user);
+
+  // const [formState, setFormState] = useState({
+  //   username: '',
+  //   email: '',
+  // });
+  // const { username, email } = formState;
+
+  // useEffect(() => {
+  //   if (data) {
+  //     setFormState({
+  //       username: user.username,
+  //       email: user.email,
+  //     });
+  //   }
+  // }, [loading, data]);
 
   return (
     <div>
@@ -30,7 +46,9 @@ export default function Profile({
         onClick={() => setShowProfile(!showProfile)}
       ></div>
 
-      <div className={`leaderboard profile modal ${showProfile && 'modal-active'}`}>
+      <div
+        className={`leaderboard profile modal ${showProfile && 'modal-active'}`}
+      >
         <button
           className="btn btn-modal"
           onClick={() => setShowProfile(!showProfile)}
@@ -44,22 +62,39 @@ export default function Profile({
 
         <form className="update-profile">
           <h3>Update Profile</h3>
-          <label className='form-label'>Username:</label>
-          <input type="text" id="username" className='form-control' defaultValue={user.username} />
-          <label className='form-label'>Email:</label>
-          <input type="text" id="email" className='form-control' defaultValue={user.email} />
-          <label className='form-label'>Password:</label>
-          <input type="password" className='form-control' id="password" />
-          <button className='btn btn-update' type="submit" disabled>
+          {/* <label className="form-label">Username:</label> */}
+          <input
+            type="text"
+            id="username"
+            className="form-control"
+            defaultValue={user.username}
+            placeholder="Username"
+          />
+          {/* <label className="form-label">Email:</label> */}
+          <input
+            type="text"
+            id="email"
+            className="form-control"
+            defaultValue={user.email}
+            placeholder="Email"
+          />
+          {/* <label className="form-label">Password:</label> */}
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+          />
+          <button className="btn btn-update" type="submit">
             Update
           </button>
         </form>
-        <div className='stats'>
+        <div className="stats">
           <h3>Stats</h3>
           <p>User stats are coming soon!</p>
         </div>
-        <div className='logout'>
-          <button className='btn btn-logout' type="logout" onClick={logout}>
+        <div className="logout">
+          <button className="btn btn-logout" type="logout" onClick={logout}>
             Logout
           </button>
         </div>
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,19 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
 
-
 export function getTimeRemaining(startedAtTime, duration) {
-  const now = new Date()
-  const then = new Date(startedAtTime)
-  const tr = Math.max(duration - Math.floor((now - then) / 1000),0)
-  return tr
+  const now = new Date();
+  const then = new Date(startedAtTime);
+  const tr = Math.max(duration - Math.floor((now - then) / 1000), 0);
+  return tr;
 }
 
 export function formatTime(rawTime) {
-  var minutes = rawTime.getMinutes()
-  var seconds = rawTime.getSeconds()
-  var strTime = `${seconds}`
+  var minutes = rawTime.getMinutes();
+  var seconds = rawTime.getSeconds();
+  var strTime = `${seconds}`;
 
-  return strTime
+  return strTime;
 }
 
 export const useIsMount = () => {
@@ -43,4 +42,10 @@ export function useInterval(callback, delay) {
       return () => clearInterval(id);
     }
   }, [delay]);
+}
+
+export function validateEmail(email) {
+  var re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }

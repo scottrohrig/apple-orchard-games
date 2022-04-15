cb0a6d6e4ec86fa79665f71fe904826a202fe2aa
Jake Pasternak <jdp.pasternak@gmail.com>
2022-04-14 18:04:17 -0600

    adds idbPromise to helpers.js

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -44,3 +44,67 @@ export function useInterval(callback, delay) {
     }
   }, [delay]);
 }
+
+// IndexedDB
+export function idbPromise(storeName, method, object) {
+  return new Promise((resolve, reject) => {
+    // Opens connection to `orchard_db` version 1
+    const request = indexedDB.open("orchard_db", 1);
+
+    // Creates variables to hold references to the database, transaction (tx), and object store
+    let db, tx, store;
+
+    // If version has changed, or first time connecting, create new object stores
+    request.onupgradeneeded = (e) => {
+      const db = request.result;
+
+      // Create object store for each type of data and set primary key to `_id` of the data
+      db.createObjectStore("orchard", { keyPath: "_id" });
+      db.createObjectStore("mashers", { keyPath: "_id" });
+      db.createObjectStore("juicers", { keyPath: "_id" });
+      db.createObjectStore("pies", { keyPath: "_id" });
+    };
+
+    // Handles errors
+    request.onerror = (e) => {
+      console.log("There was an error");
+    };
+
+    // On open success
+    request.onsuccess = (e) => {
+      // Save a reference of the database
+      db = request.result;
+      // Open a transaction with `storeName`
+      tx = db.transaction(storeName, "readwrite");
+      // Saves a reference to the object store
+      store = tx.objectStore(storeName);
+
+      // Notify of errors
+      db.onerror = (e) => {
+        console.log("error", e);
+      };
+
+      switch (method) {
+        case "put":
+          store.put(object);
+          resolve(object);
+          break;
+        case "get":
+          const all = store.getAll();
+          all.onsuccess(all.result);
+          break;
+        case "delete":
+          store.delete(object._id);
+          break;
+        default:
+          console.log("No valid method");
+          break;
+      }
+
+      // Close connection on transaction completion
+      tx.oncomplete = () => {
+        db.close();
+      };
+    };
+  });
+}

29ec92669f1f7656573a0f4752c001e7d14efe3a
jochun <jkchun@protonmail.com>
2022-04-22 08:38:54 -1000

    fixed ovenRow bug

--- client/src/components/upgrades/PlaceholderPie.jsx
+++ client/src/components/upgrades/PlaceholderPie.jsx
@@ -1,16 +1,18 @@
 import buyIcon from "../../assets/images/empty_pie.png";
 
-export default function BuyOven({ handlePurchase }) {
+export default function BuyOven({ handleUpgradePurchased }) {
   return (
-    <div className="item-container">
-      <div className="temp-img">
-        <img src={buyIcon} alt="buy upgrade" className="no-upgrade" />
-      </div>
-      <div className="item-btn-wrapper">
-        <div className="item-btn-flex">
-          <button className="btn btn-shop" onClick={handlePurchase}>
-            buy
-          </button>
+    <div className="item-box">
+      <div className="item-container">
+        <div className="temp-img">
+          <img src={buyIcon} alt="buy upgrade" className="no-upgrade" />
+        </div>
+        <div className="item-btn-wrapper">
+          <div className="item-btn-flex">
+            <button className="btn btn-shop" onClick={handleUpgradePurchased}>
+              buy
+            </button>
+          </div>
         </div>
       </div>
     </div>
